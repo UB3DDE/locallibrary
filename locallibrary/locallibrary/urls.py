4fe5722f6e74d3cc3abfc9c37fac363636bb325c
UB3DDE <32877204+UB3DDE@users.noreply.github.com>
2018-09-20 23:17:28 +0300

    Initial commit

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -14,19 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
+#from django.urls import path
+from django.conf.urls import include, url
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
 ]
 
 urlpatterns += [
-    path(r'^catalog/', include('catalog.urls')),
-    path(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
+    url(r'^catalog/', include('catalog.urls')),
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
